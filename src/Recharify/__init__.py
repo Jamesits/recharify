d2dd450a4ba1473537c41c1e4090dcf7b97321fe
James Swineson <jamesswineson@gmail.com>
2016-05-05 14:37:55 +0800

    set up i18n
    Signed-off-by: James Swineson <jamesswineson@gmail.com>

---
+++ src/Recharify/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import sys
+from PyQt5.QtWidgets import QApplication
+from Recharify.View import SplashScreen
+import gettext
+
+# Set up translation
+gettext.translation('Recharify', localedir='resources/locale/', languages=['zh_CN'], fallback=True).install()
+
+
+class App:
+    def run(self):
+        app = QApplication(sys.argv)
+        sp = SplashScreen.SplashScreen()
+        sp.show()
+        sys.exit(app.exec_())
