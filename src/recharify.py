581f775f4f43f53c2e8c6c4f506be70d20739dfc
James Swineson <jamesswineson@gmail.com>
2016-05-05 13:07:30 +0800

    First commit
    Signed-off-by: James Swineson <jamesswineson@gmail.com>

---
+++ src/recharify.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# This is startup script of Recharify.
+# https://github.com/Jamesits/recharify
+# Copyright (c) 2016 James Swineson. All rights reserved.
+
+import Recharify
+
+if __name__=="__main__":
+    pass
+
